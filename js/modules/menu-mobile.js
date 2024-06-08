e6eb17b67cbdb1f48c71ec012eafbd37eaff77e6
Raul Henrique <raul.primoimp@gmail.com>
2024-06-08 03:21:19 -0300

    correcoes gerais

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,17 +1,17 @@
-import outsideClick from './outsideclick.js';
+import outsideClick from "./outsideclick.js";
 
 export default class MenuMobile {
   constructor(menuButton, menuList, events) {
     this.menuButton = document.querySelector(menuButton);
     this.menuList = document.querySelector(menuList);
-    this.activeClass = 'active'
-    if (events === undefined) this.events = ['touchstart', 'click']
-    else this.events = events
-    this.openMenu = this.openMenu.bind(this)
+    this.activeClass = "active";
+    if (events === undefined) this.events = ["touchstart", "click"];
+    else this.events = events;
+    this.openMenu = this.openMenu.bind(this);
   }
 
-
-  openMenu() {
+  openMenu(event) {
+    event.preventDefault();
     this.menuList.classList.add(this.activeClass);
     this.menuButton.classList.add(this.activeClass);
     outsideClick(this.menuList, this.events, () => {
@@ -21,14 +21,15 @@ export default class MenuMobile {
   }
 
   addMenuMobileEvents() {
-    this.events.forEach(evento => this.menuButton.addEventListener(evento, this.openMenu));
+    this.events.forEach((evento) =>
+      this.menuButton.addEventListener(evento, this.openMenu)
+    );
   }
 
   init() {
     if (this.menuButton && this.menuList) {
-      this.addMenuMobileEvents()
+      this.addMenuMobileEvents();
     }
-    return this
+    return this;
   }
-
 }
